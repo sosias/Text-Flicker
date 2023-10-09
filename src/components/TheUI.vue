a2aa730e33cd34495aab16bce4a96c9feb8d5f4c
sosias <38495175+sosias@users.noreply.github.com>
2023-10-09 23:03:15 +0200

    add panel toggle and h keyboard shortcut

--- src/components/TheUI.vue
+++ src/components/TheUI.vue
@@ -1,6 +1,9 @@
 <script setup>
+import { ref } from 'vue'
 import { useUiStore } from '@/stores/ui'
+
 const store = useUiStore()
+const isVisible = ref(true)
 
 defineProps({
   msg: {
@@ -8,22 +11,45 @@ defineProps({
     required: true
   }
 })
+
+import { onMounted, onBeforeUnmount } from 'vue';
+
+const toggleVisibility = () => {
+    isVisible.value = !isVisible.value
+};
+
+const userReleaseKey = ({ code }) => {
+    if (code === 'KeyH') {
+        toggleVisibility()
+    }
+};
+
+onMounted(() => {
+    document.addEventListener('keyup', userReleaseKey, true)
+})
+
+onBeforeUnmount(() => {
+    document.removeEventListener('keyup', userReleaseKey, true)
+})
+
 </script>
 
 <template>
-  <div class="container">
+  <div class="container" v-if="isVisible">
     <h1 class="green">{{ msg }}</h1>
     <div>
-      <button>hide</button>
-      <input v-model.number="store.stepMilliseconds" type="range" min="1" max="1000" class="slider" />
-      <input v-model="store.stepMilliseconds" type="number" /><span>ms</span>
+      <button v-on:click="isVisible = false">hide</button><span>shortcut 'h' toggle panel</span>
+      <section>
+        <input v-model.number="store.stepMilliseconds" type="range" min="1" max="1000" class="slider" />
+        <input v-model="store.stepMilliseconds" type="number" /><span>ms</span>
+      </section>
     </div>
   </div>
 </template>
 
 <style scoped>
 .container{
-  background-color: azure;
+  background-color: #fff;
   border-radius: 5px;
 }
 </style>
