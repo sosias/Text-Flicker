0cd23e867597a8c6ef2569d9cc5a540dd34548f5
sosias <38495175+sosias@users.noreply.github.com>
2024-02-11 19:24:14 +0100

    export obsWebsocket code to a component

---
+++ src/components/ObsWebSocket.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { useUiStore } from '@/stores/ui'
+import { onMounted } from 'vue';
+
+const store = useUiStore()
+
+const changeScene = (sceneIndex) => {
+  store.wordIndex = 0
+  store.scene = sceneIndex
+}
+
+const uuidv4 = () => {
+ return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+ );
+}
+
+onMounted(() => {
+  initWebsocketConnection();
+})
+
+let sceneName = []
+let websocketAddress = "localhost:4455"
+let obsSocket
+
+const initWebsocketConnection = async() => {
+  obsSocket && obsSocket.close()
+  obsSocket = new WebSocket('ws://' + websocketAddress);
+
+  obsSocket.onopen = () => {
+    console.log('Connected to OBS WebSocket');
+  };
+
+  obsSocket.onmessage = (msg)=>{
+    console.log(msg)
+    console.log(JSON.parse(msg.data))
+    var data = JSON.parse(msg.data);
+    if(data.op == 0){ // if Hello is received identify client
+      var rpcVersion = data.d.rpcVersion
+      obsSocket.send(JSON.stringify({
+        "op": 1, // identify
+        "d": {
+          "rpcVersion": rpcVersion
+        }
+      }));
+    } else if(data.op == 2){
+      obsSocket.send(JSON.stringify({
+        "op": 6, // request
+        "d": {
+          requestType: "GetSceneList",
+          requestId: uuidv4(),
+        }
+      }));
+    } else if (data.op == 7 && data.d.requestType == "GetSceneList"){
+      sceneName = data.d.responseData.scenes.map((item)=>{return item.sceneName})
+    } 
+    else if(data.op == 5 && data.d.eventType == "CurrentProgramSceneChanged"){
+      changeScene(sceneName.indexOf(data.d.eventData.sceneName))
+    }
+  }
+
+  obsSocket.onerror = (event) => {
+      console.error("WebSocket Error:", event);
+  };
+
+  obsSocket.onclose = () => {
+    console.log('Disconnected from OBS WebSocket');
+  };
+}
+
+</script>
+
+<template>
+    <input v-model="websocketAddress" type="text" v-on:change="initWebsocketConnection()"/>
+</template>
+
+<style scoped>
+</style>
--- src/components/TheUI.vue
+++ src/components/TheUI.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import { useUiStore } from '@/stores/ui'
 import UIWordItem from './UIWordItem.vue'
+import ObsWebSocket from './ObsWebSocket.vue'
 
 const store = useUiStore()
 const isVisible = ref(true)
@@ -29,18 +30,11 @@ const selectDevice = (deviceIndex) => {
   store.wordList = wholeData[deviceIndex]
 }
 
-const uuidv4 = () => {
- return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
- );
-}
-
-let sceneName = []
 let wholeData;
 store.device = 0
 
 onMounted(() => {
-  initWebsocketConnection();
+  //initWebsocketConnection();
   fetchWords();
   document.addEventListener('keyup', userReleaseKey, true)
 })
@@ -59,54 +53,6 @@ const fetchWords = async() => {
   }
 }
 
-let websocketAddress = "localhost:4455"
-let obsSocket
-
-const initWebsocketConnection = async() => {
-  obsSocket && obsSocket.close()
-  obsSocket = new WebSocket('ws://' + websocketAddress);
-
-  obsSocket.onopen = () => {
-    console.log('Connected to OBS WebSocket');
-  };
-
-  obsSocket.onmessage = (msg)=>{
-    console.log(msg)
-    console.log(JSON.parse(msg.data))
-    var data = JSON.parse(msg.data);
-    if(data.op == 0){ // if Hello is received identify client
-      var rpcVersion = data.d.rpcVersion
-      obsSocket.send(JSON.stringify({
-        "op": 1, // identify
-        "d": {
-          "rpcVersion": rpcVersion
-        }
-      }));
-    } else if(data.op == 2){
-      obsSocket.send(JSON.stringify({
-        "op": 6, // request
-        "d": {
-          requestType: "GetSceneList",
-          requestId: uuidv4(),
-        }
-      }));
-    } else if (data.op == 7 && data.d.requestType == "GetSceneList"){
-      sceneName = data.d.responseData.scenes.map((item)=>{return item.sceneName})
-    } 
-    else if(data.op == 5 && data.d.eventType == "CurrentProgramSceneChanged"){
-      changeScene(sceneName.indexOf(data.d.eventData.sceneName))
-    }
-  }
-
-  obsSocket.onerror = (event) => {
-      console.error("WebSocket Error:", event);
-  };
-
-  obsSocket.onclose = () => {
-    console.log('Disconnected from OBS WebSocket');
-  };
-}
-
 </script>
 
 <template>
@@ -117,7 +63,7 @@ const initWebsocketConnection = async() => {
     <br/>
     Websocket address
     <section>
-      <input v-model="websocketAddress" type="text" v-on:change="initWebsocketConnection()"/>
+      <ObsWebSocket />
     </section>
     <br/>
     Device
