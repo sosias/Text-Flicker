ab39a9a246d172812cab5370065046b027f6ec15
sosias <38495175+sosias@users.noreply.github.com>
2024-02-27 00:40:52 +0100

    set fixed step to 1ms

--- src/stores/ui.js
+++ src/stores/ui.js
@@ -2,7 +2,7 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useUiStore = defineStore('ui', () => {
-  const stepMilliseconds = ref(300)
+  const stepMilliseconds = ref(1)
   const randomOffset = ref(0)
   const doubleCount = computed(() => stepMilliseconds.value * 2)
   function increment() {
